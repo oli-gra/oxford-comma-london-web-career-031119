36f89bb1a749ce5e24b9b63819f1aecb189d409b
Oliver <oliver.grant@gmail.com>
2019-01-21 01:35:16 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  str=array.join(", ")
+  i=str.rindex(",")
+  if array.length > 2
+    str[i]=", and"
+  elsif array.length > 1
+    str[i]=" and"
+  else
+  end
+  return str
 end
