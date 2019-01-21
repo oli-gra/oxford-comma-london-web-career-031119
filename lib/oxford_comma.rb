79b3997e0c8b3e0080a53e17e90f6e5c4f18f016
Oliver <oliver.grant@gmail.com>
2019-01-21 01:49:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,7 @@
 def oxford_comma(array)
   str=array.join(", ")
   i=str.rindex(",")
+  case array.length 
   if array.length > 2 { str[i]=", and" }
   if array.length = 2 { str[i]=" and" }
   return str
