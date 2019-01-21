290b9508bce2810b8658afbd8cd2db5da102176e
Oliver <oliver.grant@gmail.com>
2019-01-21 01:51:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,10 @@ def oxford_comma(array)
   str=array.join(", ")
   i=str.rindex(",")
   case array.length 
-  if array.length > 2 { str[i]=", and" }
-  if array.length = 2 { str[i]=" and" }
+  when > 2
+    str[i]=", and" 
+  when = 2
+     str[i]=" and"
+  end
   return str
 end
