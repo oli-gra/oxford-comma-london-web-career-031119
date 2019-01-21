01dde551f941b6397c8cb54bf7b75b7a7e339c97
Oliver <oliver.grant@gmail.com>
2019-01-21 01:46:21 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,7 @@
 def oxford_comma(array)
   str=array.join(", ")
   i=str.rindex(",")
-  if array.length > 2
-    str[i]=", and"
-  if array.length = 2
-    str[i]=" and"
+  if array.length > 2 { str[i]=", and" }
+  if array.length = 2 { str[i]=" and" }
   return str
 end
